6737959e2c0a6e9413d78818f7c0c25a0f0f6f8b
hbruckman <henry.bruckman@gmail.com>
2025-12-26 20:46:33 -0500

    Implement Back-end in Profile Component

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import Profile from './components/Profile/Profile';
 import Navbar from './components/Navbar/Navbar';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
@@ -19,6 +20,7 @@ function App() {
                 <Route path="/app" element={<MainPage />} />
                 <Route path="/app/login" element={<LoginPage />} />
                 <Route path="/app/register" element={<RegisterPage />} />
+                <Route path="/app/profile" element={<Profile/>} />
                 <Route path="/app/search" element={<SearchPage/>} />
                 <Route path="/app/product/:productId" element={<DetailsPage />} />
             </Routes>
